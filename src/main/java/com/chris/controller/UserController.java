c2422373b66a43f3a6081cc93c1ac3ebbb96d553
chris-tang13 <825177485@qq.com>
2023-03-04 11:42:55 +0800

    添加分页

--- src/main/java/com/chris/controller/UserController.java
+++ src/main/java/com/chris/controller/UserController.java
@@ -2,6 +2,8 @@ package com.chris.controller;
 
 import com.chris.pojo.Users;
 import com.chris.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("api")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -17,6 +20,16 @@ public class UserController {
     @RequestMapping("findall")
     public List<Users> all(){
         return userService.findAll();
+    }
+
+
+
+    @RequestMapping("findallbypage")
+    public PageInfo<Users> findAllByPage(Integer pageNum,Integer pageSize){
+        List<Users> allUsers = userService.findAll();
+        PageHelper.startPage(pageNum,pageSize);
+        PageInfo<Users> pageInfo=new PageInfo<>(allUsers);
+        return pageInfo;
     }
 
 }
